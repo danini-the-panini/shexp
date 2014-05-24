d012a16ead70b2703b41da95a4060dae1cbff04a
Daniel Smith <jellymann@gmail.com>
2014-05-24 18:27:57 +0200

    Added SHEXP code. Doesn't work yet

--- main.C
+++ main.C
@@ -13,12 +13,20 @@ double g(double t, double theta, double phi)
   return (theta >= M_PI-t) ? 0.0 : 1.0;
 }
 
+double q(double x)
+{
+  return log(x)/(x-1);
+}
+
 int main()
 {
   const int N_COEFF = N_BANDS*N_BANDS;
   typedef Matrix<double,N_COEFF,N_COEFF> MatrixSH;
+  typedef Matrix<double,N_COEFF,1> VectorSH;
   const int SQRT_N_SAMPLES = 1000;
   const int N_SAMPLES = SQRT_N_SAMPLES*SQRT_N_SAMPLES;
+  const int N_SLICES = 10;
+  const VectorSH SH_UNIT = (VectorSH() << sqrt(4.0*M_PI) , VectorXd::Zero(N_COEFF-1)).finished();
 
   SHSample* samples = new SHSample[N_SAMPLES];
   for (int i = 0; i < N_SAMPLES; i++)
@@ -27,57 +35,52 @@ int main()
   }
   SH_setup_spherical_samples(samples, SQRT_N_SAMPLES, N_BANDS);
 
-  const int N_SLICES = 10;
 
   double** slices = new double*[N_SLICES];
-  double** matrices = new double*[N_SLICES];
+  double* temp_matrix = new double[N_COEFF*N_COEFF];
 
-  int s = N_SLICES/2;
-
-  cout << "Examples:" << endl;
   for (int i = 0; i < N_SLICES; i++)
   {
-    double t = ((double)i/(double)N_SLICES)*M_PI;
+    double t = ((double)i/(double)(N_SLICES-1))*M_PI;
 
     slices[i] = new double[N_COEFF];
-    SH_project_polar_function([&] (double theta, double phi) { return g(t,theta,phi); }, samples, N_SAMPLES, N_BANDS, slices[i]);
+    SH_project_polar_function(
+        [&] (double theta, double phi)
+        {
+          return g(t,theta,phi);
+        },
+        samples, N_SAMPLES, N_BANDS, slices[i]);
 
-    matrices[i] = new double[N_COEFF*N_COEFF];
-    SH_matrix(slices[i],matrices[i]);
+    Map<VectorSH> v(slices[i]);
 
-    Map<MatrixSH, Unaligned, Stride<1,N_COEFF> > m(matrices[i]);
+    SH_matrix(slices[i],temp_matrix);
 
+    Map<MatrixSH, Unaligned, Stride<1,N_COEFF> > m(temp_matrix);
 
     SelfAdjointEigenSolver<MatrixSH> eigensolver(m);
     if (eigensolver.info() != Success) abort();
 
-    if ( i == N_COEFF/2 )
+    MatrixSH d;
+    d.setZero();
+    auto diagonal = d.diagonal();
+    for (int j =- 0; j < N_COEFF; j++)
     {
-      cout << "Original Matrix\n"
-           << m << endl;
-
-      cout << "The eigenvalues of A are:\n" << eigensolver.eigenvalues() << endl;
-      cout << "Here's a matrix whose columns are eigenvectors of A \n"
-           << "corresponding to these eigenvalues:\n"
-           << eigensolver.eigenvectors() << endl;
-
-      MatrixSH d;
-      d.setZero();
-      d.diagonal() = eigensolver.eigenvalues();
-
-      cout << "Here's something:\n"
-           << eigensolver.eigenvectors().transpose() * d * eigensolver.eigenvectors() << endl;
+      diagonal[j] = q(eigensolver.eigenvalues()[j]);
     }
 
+    MatrixSH mg = eigensolver.eigenvectors() * d * eigensolver.eigenvectors().transpose();
 
+    VectorSH f = mg * (v - SH_UNIT);
 
+    cout << "f[" << (t/M_PI)*180.0 << "] = " << f << endl;
+
+    memcpy(slices[i],&f[0],N_COEFF);
   }
 
   for (int i = 0; i < N_SLICES; i++)
   {
     delete [] slices[i];
-    delete [] matrices[i];
   }
   delete [] slices;
-  delete [] matrices;
+  delete [] temp_matrix;
 }
